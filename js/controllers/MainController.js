51536ac411b8768824c222e12251520b770fe815
efim <efim.wool@gmail.com>
2016-01-07 01:33:23 +0300

    input interface, crude cells calculation from bdate and resolution

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,22 +1,100 @@
+
 angular.module("app").controller('MainController', [function() {
 
   var vm = this;
+  vm.now = new Date();
 
-  var cellstTotal = 1080; //months in 90 y.o life
-  var cellstInPast = 288; //month in past of 24 y.o person
+  vm.countryCodes = getCountryCodesMapping();
 
-  var cells = [];
+  vm.sexCodes = getSexCodesMapping();
 
-  for (var i = 0; i < cellstInPast; i++) {
-    cells.push({
-      inPast: true
-    });
+  vm.data = {
+    birthDate: null,
+    country: null,
+    sex: null,
+    resolution: null,
+    toShow: false
+  };
+
+  TODO: 
+  vm.calculateCells = function() {
+   var lifeExpectationYears = getWHOObservation(vm.data);
+
+   var cellsData = calculateCellsData(vm.data.birthDate, lifeExpectationYears);
+
+   var cellsInPast = (vm.data.resolution == 'weeks') ? cellsData.weeksInPast : cellsData.monthsInPast;
+   var cellsInFuture = (vm.data.resolution == 'weeks') ? cellsData.weeksInFuture : cellsData.monthsInFuture;
+
+    var cells = [];
+
+    for (var i = 0; i < cellsInPast; i++) {
+      cells.push({
+        inPast: true
+      });
+    }
+    for (var i = 0; i < cellsInFuture; i++) {
+      cells.push({
+        inPast: false
+      });
+    }
+
+    vm.cells = cells;
+    vm.data.toShow = true;
   }
-  for (var i = cellstInPast; i < cellstTotal; i++) {
-    cells.push({
-      inPast: false
-    });
-  }
 
-  vm.cells = cells;
 }]);
+
+
+
+
+var getCountryCodesMapping = function() {
+  return [
+  {id: 'RU', name: "Russia"},
+  {id: 'UK', name: "United Kingdom"}
+  ];
+}
+
+var getSexCodesMapping = function() {
+  return [
+  {id: 'FMLE', name: 'Female'},
+  {id: 'MLE', name: 'Male'}
+  ];
+}
+
+// //TODO:
+// var getQueryFilterData = function() {
+//     var queryFilerData = {};
+
+//     setAgeGroup(queryFilerData);
+//     setSex(queryFilerData);
+//     setCountry(queryFilerData);
+//     setStaticFilterValues(queryFilerData);
+
+//     return queryFilerData;
+// }
+
+// //TODO:
+// var getQueryFilter = function () {
+//   //var queryFilterData = getQueryFilterData();
+//   //for property in queryFilterData
+//   //make string like COUNTRY:RUS;YEAR:2013
+// }
+
+//TODO:
+var getWHOObservation = function(userData) {
+  //get query filter from userData
+  //return call to WHO server\
+  //actually just return expected number of years left.
+  return 40;
+} 
+
+var calculateCellsData = function(birthDate, expectedYears) {
+  var cellsData = {};
+  var now = new Date();
+  cellsData.weeksInPast = (now - birthDate) / (1000 * 60 * 60 * 24 * 7);
+  cellsData.monthsInPast = (now - birthDate) / (1000 * 60 * 60 * 24 * 30.5);
+  cellsData.weeksInFuture = (expectedYears * 365) / 7;
+  cellsData.monthsInFuture = (expectedYears * 365) / 30.5;
+
+  return cellsData;
+}
